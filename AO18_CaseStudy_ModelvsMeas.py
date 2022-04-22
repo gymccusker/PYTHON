c80637c6b5ec115b345c75fb122aaf9bd074246e
gymccusker <G.Young1@leeds.ac.uk>
2022-04-22 15:35:18 +0100

    plot AP runs

--- AO18_CaseStudy_ModelvsMeas.py
+++ AO18_CaseStudy_ModelvsMeas.py
@@ -2159,8 +2159,8 @@ def main():
 
     out_dir = ['23_u-cc278_RA1M_CASIM/',
                '30_u-cg179_RA1M_CASIM/',
-               # '26_u-cd847_RA1M_CASIM/',
-               # '27_u-ce112_RA1M_CASIM/'
+               '26_u-cd847_RA1M_CASIM/',
+               '27_u-ce112_RA1M_CASIM/'
               ]
 #    out_dir = [  '30_u-cg179_RA1M_CASIM/' ]
     ### CHOOSE MONC RUNS
